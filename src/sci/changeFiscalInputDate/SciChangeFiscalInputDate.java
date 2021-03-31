5d3fb4cbbb4b5fc45ac68839e4e454ea21e1105e
Luan <Luan@DESKTOP-B4ERJ5H>
2021-03-31 11:48:00 -0300

    new modern methods

--- src/sci/changeFiscalInputDate/SciChangeFiscalInputDate.java
+++ src/sci/changeFiscalInputDate/SciChangeFiscalInputDate.java
@@ -1,15 +1,18 @@
 package sci.changeFiscalInputDate;
 
+import SimpleView.Loading;
+import fileManager.FileManager;
+import fileManager.Selector;
 import java.io.File;
 import javax.swing.JOptionPane;
-import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Pattern;
-import javax.swing.JFrame;
 import sql.Database;
 
 public class SciChangeFiscalInputDate {
 
-    private static int empresa;
+    private static Integer enterprise;
     private static File arquivo;
 
     public static void main(String[] args) {
@@ -22,15 +25,15 @@ public class SciChangeFiscalInputDate {
             }
             //Não precisa de else pois o proprio selector os informa os erros
         } else {
-            View.render("Empresa inválida!");
+            JOptionPane.showMessageDialog(null, "Empresa inválida!", "Empresa Inválida!", JOptionPane.ERROR_MESSAGE);
         }
     }
 
     private static boolean pegarArquivo() {
-        View.render("Escolha o arquivo csv com a chave do lançamento de entrada na primeira coluna e a nova data na segunda coluna:");
+        JOptionPane.showMessageDialog(null, "Escolha o arquivo csv com a chave do lançamento de entrada na primeira coluna e a nova data na segunda coluna:", "Escolha o arquivo:", JOptionPane.QUESTION_MESSAGE);
         try {
-            arquivo = Selector.Arquivo.selecionar("C:\\", "CSV", "csv");
-            return Selector.Arquivo.verifica(arquivo.getAbsolutePath(), "csv");
+            arquivo = Selector.selectFile("C:\\", "CSV", "csv");
+            return Selector.verifyFile(arquivo.getAbsolutePath(), true, "csv");
         } catch (Exception e) {
             return false;
         }
@@ -39,7 +42,7 @@ public class SciChangeFiscalInputDate {
     private static boolean pegarEmpresa() {
         String empresaStr = JOptionPane.showInputDialog("Digite o número da empresa na SCI:");
         try {
-            empresa = Integer.valueOf(empresaStr);
+            enterprise = Integer.valueOf(empresaStr);
             return true;
         } catch (Exception e) {
             return false;
@@ -48,73 +51,65 @@ public class SciChangeFiscalInputDate {
 
     private static void alterarDatas() {
         //Conecta banco
-        Banco banco = new Banco("sci.cfg");
+        Database.setStaticObject(new Database("sci.cfg"));
 
-        if (banco.testConnection()) {
-            String textoArquivo = Arquivo.ler(arquivo.getAbsolutePath());
-            String[] linhasArquivo = textoArquivo.split("\r\n");
-            int max = linhasArquivo.length;
+        if (Database.getDatabase().testConnection()) {
+            String textoArquivo = FileManager.getText(arquivo.getAbsolutePath());
+            String[] lines = textoArquivo.split("\r\n");
 
             //comeca carregamento
-            JFrame progresso = new Carregamento();
-            progresso.setVisible(true);
-            Carregamento.barra.setMinimum(0);
-            Carregamento.barra.setMaximum(max);
+            Loading loading = new Loading("Carregamento", -1, lines.length);
+            String sql = "UPDATE VEF_EMP_TMOVENT e "
+                    + "set e.BDDATAENTRADAENT = ':date' "
+                    + "where "
+                    + "e.BDCODEMP = ':enterprise' and "
+                    + "e.BDCHAVE  = ':key' ;";
+            Map<String,String> sqlSwaps = new HashMap<>();
+            sqlSwaps.put("enterprise", enterprise.toString());
 
-            //percorre linhas
-            for (int i = 0; i < max; i++) {
+            for (String line : lines) {
+                loading.next();
 
-                //atualiza barra
-                Carregamento.barra.setValue(i);
-                Carregamento.texto.setText(i + " de " + max);
-
-                String linha = linhasArquivo[i];
-                String[] colunas = linha.split(";");
-
-                int chaveEntrada;
-                Calendar data = Calendar.getInstance();
+                String[] cols = line.split(";");
 
                 try {
                     //Verifica se primeira coluna é numero
-                    chaveEntrada = Integer.valueOf(colunas[0]);
+                    String key = cols[0];
                     //verifica se primeira coluna é data valida
-                    data = getDateOfString(colunas[1]);
-                    if (data != null) {
-                        String dataSql = getDateSqlOfCalendar(data);
-                        String sql = "UPDATE VEF_EMP_TMOVENT e\n"
-                                + "set e.BDDATAENTRADAENT = '" + dataSql + "'\n"
-                                + "where \n"
-                                + "e.BDCODEMP = '" + empresa + "' and\n"
-                                + "e.BDCHAVE  = '" + chaveEntrada + "' ;";
+                    String date = getDateOfString(cols[1]);
+                    if (date != null) {
+                        sqlSwaps.put("key", key);
+                        sqlSwaps.put("date", date);
+                        
                         //executa sql
-                        banco.query(sql);
+                        Database.getDatabase().query(sql, sqlSwaps);
                     }
                 } catch (Exception e) {
+                    e.printStackTrace();
                     System.out.println("Erro: " + e);
                 }
             }
-            
-            View.render("Programa terminado! Confira as modificações na SCI.");
+
+            JOptionPane.showMessageDialog(null, "Programa terminado! Confira as modificações na SCI.");
         } else {
-            View.render("Erro ao conectar no banco de dados!");
+            JOptionPane.showMessageDialog(null, "Erro ao conectar no banco de dados!", "Erro!", JOptionPane.ERROR_MESSAGE);
         }
 
         System.exit(0);
     }
 
-    private static String getDateSqlOfCalendar(Calendar date) {
-        return date.get(Calendar.YEAR) + "-" + (date.get(Calendar.MONTH) + 1) + "-" + date.get(Calendar.DAY_OF_MONTH);
-    }
-
-    private static Calendar getDateOfString(String dataStr) {
+    private static String getDateOfString(String dataStr) {
         String regex = "^[0-3]?[0-9]\\/[0-1]?[0-9]\\/(?:[0-9]{2})[0-9]{2}$";
 
         Pattern pattern = Pattern.compile(regex);
         if (pattern.matcher(dataStr).matches()) {
-            Calendar date = Calendar.getInstance();
             try {
                 String[] dataSplit = dataStr.split("/");
-                date.set(Integer.valueOf(dataSplit[2]), Integer.valueOf(dataSplit[1]) - 1, Integer.valueOf(dataSplit[0]));
+                String date = "";
+                date += dataSplit[2] + "-";
+                date += dataSplit[1] + "-";
+                date += dataSplit[0];
+
                 return date;
             } catch (Exception e) {
                 return null;
